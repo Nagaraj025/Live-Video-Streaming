fcd3523a8a3bfb0d32a16f401e90dc058a361714
Nagaraj025 <47721585+Nagaraj025@users.noreply.github.com>
2020-12-03 15:54:31 +0530

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Stop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(262, 359);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(82, 32);
+            this.Stop.TabIndex = 0;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(51, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(440, 320);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(110, 359);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 32);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(488, 359);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 420);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Stop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+using AForge.Controls;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+
+        private VideoCaptureDevice FinalVideo = null;
+        private VideoCaptureDeviceForm captureDevice;
+        private Bitmap video;
+        //private Vide
+        Server ServerObject = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            captureDevice = new VideoCaptureDeviceForm();
+            //Process.Start("https://javisystems.keka.com/ui/#/me/attendance/logs");
+            ServerObject = new Server(this);
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public void Addlog(String msg)
+        {
+            this.Invoke((MethodInvoker)delegate
+           {
+               textBox1.Text += msg;
+           });
+            
+        }
+        public Bitmap  bitmap_img()
+        {
+            //this.Invoke((MethodInvoker)delegate
+            //{
+            //    return video;
+            //});
+            return video;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (captureDevice.ShowDialog(this) == DialogResult.OK)
+            {
+                FinalVideo = captureDevice.VideoDevice;
+                FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+                FinalVideo.Start();
+            }
+            ServerObject.Start();
+        }
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            video = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(FinalVideo == null)
+            {
+                //Application.Exit();
+                
+            }
+            if (FinalVideo.IsRunning)
+            {
+                this.FinalVideo.Stop();
+                pictureBox1.Image = null;
+            }
+
+            
+        }
+
+        
+    }
+}
